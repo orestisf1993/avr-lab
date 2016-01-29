843d44099697bdbdd59ac1d82c2711efb7c2f17d
orestisf <orestisf1993@gmail.com>
2016-01-30 00:35:19 +0200

    Add exercise 1

--- labs-exam/labs-exam.asm
+++ labs-exam/labs-exam.asm
@@ -20,3 +20,23 @@ ldi r25,high(RAMEND)
 out sph,r25
 
 rcall test_succeeded
+
+; Exercise 1
+ex1:
+.def counter=r16
+.def value=r17
+#define INITIAL_POS 0x60
+#define MY_VALUE 0x5f
+#define REPEAT_COUNT 64
+ldi XL, low(INITIAL_POS)
+ldi XH, high(INITIAL_POS)
+ldi counter, REPEAT_COUNT
+ldi value, MY_VALUE
+loop1:
+st X+, value
+dec counter
+tst counter
+brne loop1
+ret
+.undef counter
+.undef value
